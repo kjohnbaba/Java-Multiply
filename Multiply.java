276363559a3050a4345505deb4bc77ea8e3857e5
kjohnbaba <105748315+kjohnbaba@users.noreply.github.com>
2022-06-02 17:10:11 -0400

    Update Multiply.java

--- Multiply.java
+++ Multiply.java
@@ -1,11 +1,13 @@
 public class Multiply {
-    public static void main(String[] args)
-    {
+    
+    public static void main(String[] args){
+        
     StdOut.print("Type an integer to multiply: ");
     int x = StdIn.readInt();
     StdOut.print("Type the second integer: ");
     int y = StdIn.readInt();
     int sum = x * y;
     StdOut.println("Their multiplication is " + sum);
+        
     }
 }
